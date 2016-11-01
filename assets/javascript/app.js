3b4c73e6a34caa16c8ab2fe18d110bf33b8140f5
Tara Baker <Tara Baker>
2016-11-01 07:27:59 -0400

    added js to find status of radios

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,72 @@
 
 //==================PSEUDO-CODE===================================
 
+
+var correctAnswers = 0;
+//the questions should be hidden when the game loads
+//$('#question_one').hide();
+//$('#question_two').hide();
+//$('#next_question').hide();
 //user presses start button, and the time starts
-//<button onclick="startTimer()">Try it</button>
+
+//check if clicked radio matches answers array (correct answer), if so, it adds 1 to the correct answers
+function checkAnswer(checkedValue) {
+  var answers =["Nevada", "Wisconsin"];
+  for (var b = 0; b < answers.length; b++) {
+      if (answers[b] == checkedValue){
+        correctAnswers++;
+
+      }
+  }
+}
+
+//returns the clicke
+function getAnswer(radioName) {
 
 
-$('#start').on('click', function(startTimer) {
+  var radioButtons = document.getElementsByName((radioName).toString()); 
+  for (var x = 0; x < radioButtons.length; x++) {
+  	if (radioButtons[x].checked) {
+  		console.log("getAnswer is running");
+  		return radioButtons[x].value;
+
+  	}
+  }
+}
+
+$('#start').on('click', function() {
+//	$('#start').hide();
+	startTimer();
 	console.log("started");
 	var myInterval;
 
     function startTimer () {
-    	myInterval = setInterval(hello, 3000);
+    	myInterval = setTimeout(intervalEnd, 3000);
+    	alert("myInterval");
     }
-    function hello() {
-    	alert("hello");
+    function intervalEnd() {
+    	alert("Time's Up!");
     }
+
+// call the function getAnswer twice.
+ var clickedValue = "";
+ for (var n = 1; n < 3; n++) {
+    clickedValue = getAnswer(n);
+    //if something is checked (not empty) then check if it is correct
+    if (clickedValue != null) {
+        checkAnswer(clickedValue);
+    }
+  }
+  alert(correctAnswers);
+
+
+    //question one should appear after the button is clicked
+//    $('#question_one').show();
+ //   $('#next_question').show();
 });
+
 //display results when all questions are answered, or when timer runs out
 //user makes a selection
+
 //record whether selection was correct answer or not, stor in wins and looses variables
 //repeat previous two steps for all the questions in the quiz
