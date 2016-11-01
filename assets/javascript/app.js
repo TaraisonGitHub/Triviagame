7c8aea2970157fbce7629559006f07eebc2d1065
Tara Baker <Tara Baker>
2016-11-01 07:58:13 -0400

    commented code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,26 +10,26 @@ var correctAnswers = 0;
 //user presses start button, and the time starts
 
 //check if clicked radio matches answers array (correct answer), if so, it adds 1 to the correct answers
-function checkAnswer(checkedValue) {
-  var answers =["Nevada", "Wisconsin"];
+function checkAnswer(checkedValue) { //checkedValue is a variable being passed int to the function CheckAnswer
+  var answers =["Nevada", "Wisconsin"]; //these are the correct answers in an array
   for (var b = 0; b < answers.length; b++) {
-      if (answers[b] == checkedValue){
+      if (answers[b] == checkedValue){// if any of the correct answers matches the checkedValue, 
         correctAnswers++;
 
       }
   }
 }
 
-//returns the clicke
-function getAnswer(radioName) {
+//iterate through each radio button group and see if the status of any of them is "checked"
+function getAnswer(radioName) { //radoName is just a variable being passed into the function getAnswer
+//getAnswer is called on line 54
 
-
-  var radioButtons = document.getElementsByName((radioName).toString()); 
-  for (var x = 0; x < radioButtons.length; x++) {
+  var radioButtons = document.getElementsByName((radioName).toString()); //radioName checks the groups by their name, which is 1 or 2, and converts that into a String
+  for (var x = 0; x < radioButtons.length; x++) {//iterate ("count") all the radio buttons
   	if (radioButtons[x].checked) {
   		console.log("getAnswer is running");
-  		return radioButtons[x].value;
-
+  		return radioButtons[x].value; //the function stops running, and returns the value of the checked radio to you. It does this once for each group.
+      //if "Texas", is checked, this is saying "I looked at all the radio buttons, and the one with the value 'Texas' is checked"
   	}
   }
 }
